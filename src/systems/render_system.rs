43ca3604d55e03e5124eeec9bb1e1cedc509cfca
Liby Lee <liby99@icloud.com>
2019-09-14 13:22:10 -0400

    Refractoring render system

--- src/systems/render_system.rs
+++ src/systems/render_system.rs
@@ -7,6 +7,64 @@ use crate::components::{
   line::{Line, LineStyle},
 };
 
+fn draw_line(line: &Line, style: &LineStyle, context: Context, graphics: &mut G2d) {
+  let x_min = -480.;
+  let x_max  = -x_min;
+  let y_min  = -360.;
+  let y_max = -y_min;
+
+  let (p1, p2) = if line.direction.x == 0. {
+    if line.origin.y >= y_min && line.origin.y <= y_max {
+      (Some([line.origin.x, y_min]), Some([line.origin.x, y_max]))
+    } else {
+      (None, None)
+    }
+  } else if line.direction.y == 0. {
+    if line.origin.y >= y_min && line.origin.y <= y_max {
+      (Some([x_min, line.origin.y]), Some([x_max, line.origin.y]))
+    } else {
+      (None, None)
+    }
+  } else {
+    let bottom_x = line.origin.x + (y_min - line.origin.y) / line.direction.y * line.direction.x;
+    let top_x = line.origin.x + (y_max - line.origin.y) / line.direction.y * line.direction.x;
+    let left_y = line.origin.y + (x_min - line.origin.x) / line.direction.x * line.direction.y;
+    let right_y = line.origin.y + (x_max - line.origin.x) / line.direction.x * line.direction.y;
+
+    let bottom = bottom_x >= x_min && bottom_x <= x_max;
+    let top = top_x >= x_min && top_x <= x_max;
+    let left = left_y >= y_min && left_y <= y_max;
+    let right = right_y >= y_min && right_y <= y_max;
+
+    let mut p1 = None;
+    let mut p2 = None;
+
+    if bottom { p1 = Some([bottom_x, y_min]); }
+    if top { if p1.is_some() { p2 = Some([top_x, y_max]); } else { p1 = Some([top_x, y_max]); } }
+    if left { if p1.is_some() { p2 = Some([left_y, x_min]); } else { p1 = Some([left_y, x_min]); } }
+    if right { if p1.is_some() { p2 = Some([left_y, x_max]); } else { p1 = Some([left_y, x_max]); } }
+
+    (p1, p2)
+  };
+
+  match (p1, p2) {
+    (Some(from), Some(to)) => {
+      line_from_to(style.color.into(), style.width, [480. + from[0], 360. - from[1]], [480. + to[0], 360. - to[1]], context.transform, graphics);
+    },
+    _ => ()
+  }
+}
+
+fn draw_point(point: &Point, style: &PointStyle, context: Context, graphics: &mut G2d) {
+  let Point(pos) = point;
+  ellipse(
+    style.color.into(),
+    [480. + pos.x - style.radius, 360. - pos.y - style.radius, style.radius * 2., style.radius * 2.],
+    context.transform,
+    graphics,
+  );
+}
+
 pub struct RenderSystem {
   pub window: PistonWindow,
 }
@@ -31,71 +89,14 @@ impl<'a> System<'a> for RenderSystem {
       self.window.draw_2d(&event, |context, graphics, _device| {
         clear(Color::white().into(), graphics);
 
-        let x_min = -480.;
-        let x_max = -x_min;
-        let y_min = -360.;
-        let y_max = -y_min;
-
+        // Fisrt draw lines
         for (line, style) in (&lines, &line_styles).join() {
-
-          let (p1, p2) = if line.direction.x == 0. {
-            if line.origin.y >= y_min && line.origin.y <= y_max {
-              (Some([line.origin.x, y_min]), Some([line.origin.x, y_max]))
-            } else {
-              (None, None)
-            }
-          } else if line.direction.y == 0. {
-            if line.origin.y >= y_min && line.origin.y <= y_max {
-              (Some([x_min, line.origin.y]), Some([x_max, line.origin.y]))
-            } else {
-              (None, None)
-            }
-          } else {
-            let bottom_x = line.origin.x + (y_min - line.origin.y) / line.direction.y * line.direction.x;
-            let top_x = line.origin.x + (y_max - line.origin.y) / line.direction.y * line.direction.x;
-            let left_y = line.origin.y + (x_min - line.origin.x) / line.direction.x * line.direction.y;
-            let right_y = line.origin.y + (x_max - line.origin.x) / line.direction.x * line.direction.y;
-
-            let bottom = bottom_x >= x_min && bottom_x <= x_max;
-            let top = top_x >= x_min && top_x <= x_max;
-            let left = left_y >= y_min && left_y <= y_max;
-            let right = right_y >= y_min && right_y <= y_max;
-
-            let mut p1 = None;
-            let mut p2 = None;
-
-            if bottom { p1 = Some([bottom_x, y_min]); }
-            if top { if p1.is_some() { p2 = Some([top_x, y_max]); } else { p1 = Some([top_x, y_max]); } }
-            if left { if p1.is_some() { p2 = Some([left_y, x_min]); } else { p1 = Some([left_y, x_min]); } }
-            if right { if p1.is_some() { p2 = Some([left_y, x_max]); } else { p1 = Some([left_y, x_max]); } }
-
-            (p1, p2)
-          };
-
-          match (p1, p2) {
-            (Some(from), Some(to)) => {
-              line_from_to(
-                style.color.into(),
-                style.width,
-                [480. + from[0], 360. - from[1]],
-                [480. + to[0], 360. - to[1]],
-                context.transform,
-                graphics,
-              );
-            },
-            _ => ()
-          }
+          draw_line(line, style, context, graphics);
         }
 
-        // Draw points
+        // Then draw points (as points are on top of lines)
         for (point, style) in (&points, &point_styles).join() {
-          let pos = point.0;
-          ellipse(
-            style.color.into(),
-            [480. + pos.x - style.radius, 360. - pos.y - style.radius, style.radius * 2., style.radius * 2.],
-            context.transform,
-            graphics,
-          );
+          draw_point(point, style, context, graphics);
         }
       });
     } else {
