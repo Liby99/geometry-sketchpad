17208fcee9a725227285e0d4684a60cdbdb1638d
Liby Lee <liby99@icloud.com>
2019-09-15 19:00:52 -0400

    Tested spatial hash table. Still need more test

--- src/resources/spatial_hash_table.rs
+++ src/resources/spatial_hash_table.rs
@@ -40,15 +40,11 @@ impl<T: Clone + Eq + Hash> SpatialHashTable<T> {
     let aabb = vp.actual_aabb();
     if let Some((p1, p2)) = l.to_actual(vp).intersect(aabb) {
       println!("p1: {:?}, p2: {:?}", p1, p2);
-      let (init_x_tile, init_y_tile) = self.get_unlimited_cell(p1, vp);
-      let (end_x_tile, end_y_tile) = self.get_unlimited_cell(p2, vp);
+      let (init_x_tile, init_y_tile) = self.get_unlimited_cell(p1);
+      let (end_x_tile, end_y_tile) = self.get_unlimited_cell(p2);
       if init_x_tile == end_x_tile {
-        println!("init_x_tile: {}", init_x_tile);
         if 0 <= init_x_tile && init_x_tile < self.x_tiles as i64 {
-          // println!("init_x_tile: {}", init_x_tile);
-          println!("Here");
           for y_tile in 0..self.y_tiles {
-            println!("{}", y_tile);
             let tile = self.get_cell_by_x_y(init_x_tile as usize, y_tile);
             self.table[tile].push(ent.clone());
           }
@@ -63,19 +59,23 @@ impl<T: Clone + Eq + Hash> SpatialHashTable<T> {
       } else {
         // Making sure p1 to p2 is from left to right
         let (p1, p2) = if p1.x > p2.x { (p2, p1) } else { (p1, p2) };
+        let (init_x_tile, init_y_tile) = self.get_unlimited_cell(p1);
+        println!("{:?}, {:?}", p1, p2);
         let dir = (p2 - p1).normalized();
         if dir.y < 0.0 {
+          // println!("<");
           let mut curr_x = p1.x;
           let mut curr_y = p1.y;
           let mut curr_x_tile = init_x_tile as i64;
           let mut curr_y_tile = init_y_tile as i64;
-          while 0 <= curr_x_tile && curr_x_tile < self.x_tiles as i64 && 0 <= curr_y_tile && curr_y_tile <= self.y_tiles as i64 {
+          while 0 <= curr_x_tile && curr_x_tile < self.x_tiles as i64 && 0 <= curr_y_tile && curr_y_tile < self.y_tiles as i64 {
             let tile_offset_y = curr_y - curr_y_tile as f64 * TILE_SIZE;
             let next_y = curr_y_tile as f64 * TILE_SIZE;
             let next_x_diff = tile_offset_y / dir.y.abs() * dir.x;
             let next_x = curr_x + next_x_diff;
             let next_x_tile = (next_x / TILE_SIZE) as i64;
-            for tile_x in curr_x_tile..next_x_tile {
+            // println!("curr_x {}, curr_y {}, curr_x_tile {}, curr_y_tile {}, next_x_tile {}", curr_x, curr_y, curr_x_tile, curr_y_tile, next_x_tile);
+            for tile_x in curr_x_tile..(next_x_tile + 1) {
               if tile_x < self.x_tiles as i64 {
                 let tile = self.get_cell_by_x_y(tile_x as usize, curr_y_tile as usize);
                 self.table[tile].push(ent.clone());
@@ -91,13 +91,13 @@ impl<T: Clone + Eq + Hash> SpatialHashTable<T> {
           let mut curr_y = p1.y;
           let mut curr_x_tile = init_x_tile as i64;
           let mut curr_y_tile = init_y_tile as i64;
-          while 0 <= curr_x_tile && curr_x_tile < self.x_tiles as i64 && 0 <= curr_y_tile && curr_y_tile <= self.y_tiles as i64 {
+          while 0 <= curr_x_tile && curr_x_tile < self.x_tiles as i64 && 0 <= curr_y_tile && curr_y_tile < self.y_tiles as i64 {
             let next_y = (curr_y_tile + 1) as f64 * TILE_SIZE;
             let offset_y = next_y - curr_y;
             let next_x_diff = offset_y / dir.y.abs() * dir.x;
             let next_x = curr_x + next_x_diff;
             let next_x_tile = (next_x / TILE_SIZE) as i64;
-            for tile_x in curr_x_tile..next_x_tile {
+            for tile_x in curr_x_tile..(next_x_tile + 1) {
               if tile_x < self.x_tiles as i64 {
                 let tile = self.get_cell_by_x_y(tile_x as usize, curr_y_tile as usize);
                 self.table[tile].push(ent.clone());
@@ -126,7 +126,7 @@ impl<T: Clone + Eq + Hash> SpatialHashTable<T> {
     // println!("(x: {}, x_tile: {}, y: {}, y_tile: {}, TILE_SIZE: {})", x, x_tile, y, y_tile, TILE_SIZE);
   }
 
-  fn get_unlimited_cell(&self, p: Point, vp: &Viewport) -> (i64, i64) {
+  fn get_unlimited_cell(&self, p: Point) -> (i64, i64) {
     let Vector2 { x, y } = p;
     let x_tile = (x / TILE_SIZE).floor() as i64;
     let y_tile = (y / TILE_SIZE).floor() as i64;
@@ -245,5 +245,66 @@ mod tests {
     assert!(table.table[1] == vec![0]);
     assert!(table.table[2] == vec![0]);
     assert!(table.table[3].is_empty());
+  }
+
+  /// + - - + - - +
+  /// |     |     |
+  /// | \   |     |
+  /// + - - + - - +
+  /// |    \|     |
+  /// |     |\    |
+  /// + - - + - - +
+  #[test]
+  fn test_insert_line_3() {
+    let vp = &Viewport::new(vec2![0., 0.], vec2![2., 2.], vec2![80., 80.]); // 田
+    let mut table : SpatialHashTable<i32> = SpatialHashTable::default();
+    table.init_viewport(vp);
+
+    let l = Line { origin: vec2![-0.5, 0.0], direction: vec2![(2.0 as f64).sqrt(), -(2.0 as f64).sqrt()] };
+    table.insert_line(0, l, vp);
+
+    assert!(table.table[0] == vec![0]);
+    assert!(table.table[1].is_empty());
+    assert!(table.table[2] == vec![0]);
+    assert!(table.table[3] == vec![0]);
+  }
+
+  #[test]
+  fn test_insert_line_4() {
+    let vp = &Viewport::new(vec2![0., 0.], vec2![4., 4.], vec2![160., 160.]); // 田
+    let mut table : SpatialHashTable<i32> = SpatialHashTable::default();
+    table.init_viewport(vp);
+
+    let l = Line { origin: vec2![-0.5, 0.0], direction: vec2![(2.0 as f64).sqrt(), (2.0 as f64).sqrt()] };
+    table.insert_line(0, l, vp);
+
+    println!("{:?}", table);
+
+    for i in 0..16 {
+      match i {
+        2 | 3 | 5 | 6 | 8 | 9 | 12 => assert!(table.table[i] == vec![0]),
+        _ => assert!(table.table[i].is_empty())
+      }
+    }
+  }
+
+  #[test]
+  fn test_insert_line_5() {
+    let vp = &Viewport::new(vec2![0., 0.], vec2![4., 4.], vec2![160., 160.]); // 田
+    let mut table : SpatialHashTable<i32> = SpatialHashTable::default();
+    table.init_viewport(vp);
+
+    let sqrt17 = (17.0 as f64).sqrt();
+    let l = Line { origin: vec2![0.0, 0.0], direction: vec2![4.0 / sqrt17, 1.0 / sqrt17] };
+    table.insert_line(0, l, vp);
+
+    println!("{:?}", table);
+
+    for i in 0..16 {
+      match i {
+        6 | 7 | 8 | 9 | 10 => assert!(table.table[i] == vec![0]),
+        _ => assert!(table.table[i].is_empty())
+      }
+    }
   }
 }
