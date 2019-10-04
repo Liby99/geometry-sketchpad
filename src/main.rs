4a60e2927eb8d8db7488e4dfc5be92f6623540b9
Liby Lee <liby99@icloud.com>
2019-10-03 22:08:56 -0400

    Can run

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ fn main() {
 
   // Create dispatcher
   let mut dispatcher = DispatcherBuilder::new()
+    .with(SolverSystem, "solver_system", &[])
     .with_thread_local(window_system)
     .build();
 
